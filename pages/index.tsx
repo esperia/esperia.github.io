1644136177fbf3e5a9fdfb2c2e1741accdbb363c
neske <esperia09@gmail.com>
2020-04-11 23:55:37 +0900

    アラートテストを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => (
   <Layout>
     <h1>Tests</h1>
     <ul>
-      <li><Link href='/tests/webgl/example' as={process.env.BACKEND_URL + '/tests/webgl/example'}><a>WebGL</a></Link></li>
+      <li><Link href='/tests/webgl/study/test' as={process.env.BACKEND_URL + '/tests/webgl/study/test'}><a>WebGL</a></Link></li>
     </ul>
   </Layout>
 )
---
+++ pages/tests/webgl/study/test.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Test = () => {
+  alert(1)
+  return (
+    <h1>Test</h1>
+  )
+}
+
+export default Test
