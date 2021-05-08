ca058d30453e544a6d49e0482f00cbe101c6187f
Satoshi Kono <s.kono@subthread.co.jp>
2021-05-09 02:37:48 +0900

    Delete old contents

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app'
-import '../styles.scss'
-
-// This default export is required in a new `pages/_app.js` file.
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
